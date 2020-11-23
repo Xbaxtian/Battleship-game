a7299340962b227fee8484520cadab862e3536b4
Xbaxtian <xbaxtian@hotmail.com>
2020-11-23 09:31:46 -0500

    initial config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+MyApp.propTypes = {
+  pageProps: PropTypes.shape({}).isRequired,
+  Component: PropTypes.elementType.isRequired,
+};
 
 export default MyApp
